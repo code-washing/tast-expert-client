8fb0513c9ab58b95666fbac2185eb8ea13c187b6
code-washing <tadcassin225@gmail.com>
2023-12-22 05:48:11 +0600

    Include DashboardHeader component in TaskDashboardLayout component

--- src/components/layouts/TaskDashboardLayout.jsx
+++ src/components/layouts/TaskDashboardLayout.jsx
@@ -1,9 +1,13 @@
 // react-router imports
 import { Outlet } from "react-router-dom";
 
+// dashboard header
+import DashboardHeader from "../shared/DashboardHeader/DashboardHeader";
+
 function TaskDashboardLayout() {
   return (
     <div>
+      <DashboardHeader />
       <Outlet />
     </div>
   );
