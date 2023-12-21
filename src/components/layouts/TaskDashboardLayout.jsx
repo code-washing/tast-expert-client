da65d785cdd7050612db605a30128e1498b158d7
code-washing <tadcassin225@gmail.com>
2023-12-21 22:16:51 +0600

    Update TaskManagementLayout, create TaskManagement page, and add both of them to the main router

--- src/components/layouts/TaskDashboardLayout.jsx
+++ src/components/layouts/TaskDashboardLayout.jsx
@@ -1,5 +1,12 @@
-const TaskDashboardLayout = () => {
-  return <div></div>;
-};
+// react-router imports
+import { Outlet } from "react-router-dom";
+
+function TaskDashboardLayout() {
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+}
 
 export default TaskDashboardLayout;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,15 +3,22 @@ import { createBrowserRouter } from "react-router-dom";
 
 // main layout
 import RootLayout from "../components/layouts/RootLayout";
+import TaskDashboardLayout from "../components/layouts/TaskDashboardLayout";
 
 // page components
 import Home from "../components/pages/Home/Home";
+import TaskManagement from "../components/pages/TaskManagement/TaskManagement";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     children: [{ path: "/", element: <Home /> }],
+  },
+  {
+    path: "/task-management",
+    element: <TaskDashboardLayout />,
+    children: [{ path: "/tast-management", element: <TaskManagement /> }],
   },
 ]);
 
