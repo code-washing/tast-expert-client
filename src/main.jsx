c5e9244927748e2254e7f4052278765cdef8c6ac
code-washing <tadcassin225@gmail.com>
2023-12-21 20:03:39 +0600

    Add Toastify container and imports to the main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,30 @@ import { RouterProvider } from "react-router-dom";
 // router import
 import router from "./router/router";
 
+// react toastify
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Slide } from "react-toastify";
+
 // style import
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    {/* react toastify */}
+    <ToastContainer
+      position="top-center"
+      autoClose={2000}
+      transition={Slide}
+      hideProgressBar
+      newestOnTop
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
     <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
