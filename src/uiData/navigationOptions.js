764fb1a54782ccd24aa05f7a65360e0637b23625
code-washing <tadcassin225@gmail.com>
2023-12-22 05:47:18 +0600

    Add uiData folder to project and add navigation options data inside

---
+++ src/uiData/navigationOptions.js
@@ -0,0 +1,6 @@
+export const navOptions = [
+  { id: 0, text: "Home", url: "/" },
+  { id: 1, text: "Apartments", url: "/apartments" },
+  { id: 2, text: "Features", url: "/#features", hashed: true },
+  { id: 3, text: "Directions", url: "/#directions", hashed: true },
+];
