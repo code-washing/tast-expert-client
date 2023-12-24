42d9796a5caf3958e026ff17b1d289a42eef0bf3
code-washing <tadcassin225@gmail.com>
2023-12-24 22:23:01 +0600

    Add login and register routes to the router and add a custom animation riseUp to the index.css

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,24 +1,37 @@
 // react router imports
 import { createBrowserRouter } from "react-router-dom";
 
-// main layout
+// layout
 import RootLayout from "../components/layouts/RootLayout";
 import TaskDashboardLayout from "../components/layouts/TaskDashboardLayout";
+import AuthLayout from "../components/layouts/AuthLayout";
 
 // page components
 import Home from "../components/pages/Home/Home";
 import TaskManagement from "../components/pages/TaskManagement/TaskManagement";
+import LoginPage from "../components/pages/LoginPage/LoginPage";
+import RegisterPage from "../components/pages/RegisterPage/RegisterPage";
+import ErrorPage from "./../components/pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     children: [{ path: "/", element: <Home /> }],
+    errorElement: <ErrorPage />,
   },
   {
     path: "/task-management",
     element: <TaskDashboardLayout />,
     children: [{ path: "/task-management", element: <TaskManagement /> }],
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [
+      { path: "login", element: <LoginPage /> },
+      { path: "register", element: <RegisterPage /> },
+    ],
   },
 ]);
 
