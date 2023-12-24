cc6d46e3ee439dc262dcb2209257d0ad7e2d884d
code-washing <tadcassin225@gmail.com>
2023-12-24 21:56:33 +0600

    Add image for the login/register forms and banner image to assets

---
+++ src/uiData/authUiContent.jsx
@@ -0,0 +1,4 @@
+// auth image
+import developersImage from "./../assets/auth/auth.webp";
+
+export const authImage = developersImage;
---
+++ src/uiData/homeUiContent.jsx
@@ -0,0 +1,21 @@
+// top Banner image
+import bannerImage from "./../assets/banner/banner.webp.webp";
+
+export const homeTopBannerTextContent = {
+  heading: (
+    <>
+      Task Expert: Streamlining Your{" "}
+      <span className="text-primary">Productivity</span>
+    </>
+  ),
+  subheading: <>Effortless Task Management for a Seamless Workflow</>,
+};
+
+export const homeBannerImage = bannerImage;
+
+export const introductionData = {
+  fullname: "Nashiuz Zaman",
+  designation: "Web Application Developer",
+  description:
+    "I'm a junior MERN stack developer who primarily develops fully responsive websites using React JS, Redux toolkit, Tanstack Query, Tailwind CSS, Sass, Firebase Authentication, Express JS, MongoDB and Mongoose. I also possess moderate server-side development skills in Vanilla PHP and MySQL. I am proficient in Object Oriented Programming for both JavaScript and PHP. My current goal is to learn Laravel framework after I've finished exploring Node JS, MongoDB and Mongoose.",
+};
