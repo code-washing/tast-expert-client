8c0ee6639405de7af592cf7b96532dc3c87ebfb7
code-washing <tadcassin225@gmail.com>
2023-12-28 14:03:27 +0600

    Transfer ToastContainer and BackdropBlur components from Main.jsx to PrimaryComponent

--- src/components/PrimaryComponent/PrimaryComponent.jsx
+++ src/components/PrimaryComponent/PrimaryComponent.jsx
@@ -1,8 +1,38 @@
 import PropTypes from "prop-types";
 
+// react toastify
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Slide } from "react-toastify";
+
+// components
+import BackdropBlur from "../shared/BackdropBlur/BackdropBlur";
+
+// redux
+import { useSelector } from "react-redux";
+
 const PrimaryComponent = ({ children }) => {
+  const { open } = useSelector((store) => store.backdrop);
+
   return (
     <div className="font-default min-h-screen flex flex-col max-w-[120rem] mx-auto overflow-x-hidden">
+      {/* react toastify */}
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        transition={Slide}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      {/* blur overlay in the page */}
+      <BackdropBlur openState={open} />
+
       {children}
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,6 @@ import { RouterProvider } from "react-router-dom";
 // router import
 import router from "./router/router";
 
-// react toastify
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { Slide } from "react-toastify";
-
 // redux-toolkit
 import { store } from "./app/store";
 import { Provider } from "react-redux";
@@ -31,20 +26,6 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* react toastify */}
-    <ToastContainer
-      position="top-center"
-      autoClose={2000}
-      transition={Slide}
-      hideProgressBar
-      newestOnTop
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="light"
-    />
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         {/* main app div*/}
