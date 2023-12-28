50dcdec1d532597891279ba2ace95d5155301dc4
code-washing <tadcassin225@gmail.com>
2023-12-28 14:03:53 +0600

    Add backdropReducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -6,6 +6,7 @@ import authReducer from "./../features/auth/authSlice";
 import mobileNavReducer from "./../features/mobileNav/mobileNavSlice";
 import taskReducer from "./../features/task/taskSlice";
 import dragDropReducer from "./../features/dragDrop/dragDropSlice";
+import backdropReducer from "./../features/backdrop/backdropSlice";
 
 export const store = configureStore({
   reducer: {
@@ -14,5 +15,6 @@ export const store = configureStore({
     mobileNav: mobileNavReducer,
     task: taskReducer,
     dragDrop: dragDropReducer,
+    backdrop: backdropReducer,
   },
 });
