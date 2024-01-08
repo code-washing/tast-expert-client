54cf5a00fb316fc56909fcc8a5ab4094952fcae5
code-washing <tadcassin225@gmail.com>
2024-01-08 23:23:09 +0600

    Change styles of LoadingSpinner component

--- src/components/shared/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/shared/LoadingSpinner/LoadingSpinner.jsx
@@ -13,11 +13,13 @@ const LoadingSpinner = ({
     <div className={`${modifyClasses}`}>
       <div className="flex flex-col md:flex-row justify-center items-center gap-3">
         {/* text */}
-        {!onlyLoader && <div style={{ fontSize: "inherit" }}>{text}</div>}
+        {!onlyLoader && (
+          <div style={{ fontSize: "inherit", color: "inherit" }}>{text}</div>
+        )}
 
         {/* loading spinner */}
         <ImSpinner8
-          style={{ fontSize: "inherit" }}
+          style={{ fontSize: "inherit", color: "inherit" }}
           className="text-primaryLight animate-spin font-bold"
         />
       </div>
